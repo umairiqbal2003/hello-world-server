97d909e041e1a61beba18c65df5f773a32d966c8
Umair Iqbal <umairazeem1999@gmail.com>
2022-08-04 00:36:43 +0500

    last commit for server

--- server.mjs
+++ server.mjs
@@ -1,11 +1,46 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+
+app.get('/home', (req, res) => {
+  res.send('Hello I am homepage!')
 })
+app.get('/profilr', (req, res) => {
+    res.send('Hello I am Profile Page!')
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  app.get('/weather', (req, res) => {
+    res.send({
+      city : "karachi" , 
+      temperature : "28",
+      condition  : "Rainy System Enters",
+      localtime : "09-03-2022",
+      country  : "Pakistan",
+      developer : "Made By Umair and taught by Sir Inzemam Malik"
+    },{
+        city : "london" , 
+        temperature : "23",
+        condition : "Cloudy",
+        localtime : "09-03-2022 12:00 AM",
+        country  : "Pakistan",
+      developer : "Made By Umair taught by Sir Inzemam Malik"
+
+      }
+      ,{
+        city : "turkey" , 
+        temperature : "24",
+        condition : "Partialy Cloudy",
+        localtime : "09-03-2022 9:00 PM",
+        country  : "Pakistan",
+      developer : "Made By Umair and taught by Sir Inzemam Malik"
+
+      })
+  })
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`)
 })
