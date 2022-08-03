6c3651c26e4973f9cf857313a152132052677384
Umair Iqbal <umairazeem1999@gmail.com>
2022-08-04 00:43:27 +0500

    complete hello server

--- server.mjs
+++ server.mjs
@@ -41,6 +41,9 @@ app.get('/profilr', (req, res) => {
 
       })
   })
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
+
+ const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
